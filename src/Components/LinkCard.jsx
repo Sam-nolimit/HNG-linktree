f52d128130abbd46243a46192ba3a94189ea16f5
Temitope Adejolu <tadejolu@gmail.com>
2022-10-28 00:01:13 +0100

    updated with declaratives

--- src/Components/LinkCard.jsx
+++ src/Components/LinkCard.jsx
@@ -2,19 +2,19 @@ import { HoverEffectStyle, LinkCardStyle } from "./LinkCardStyles";
 
 const LinkCard = () => {
   let links = [
-    { name: "Twitter Link", link: "https://twitter.com/TAdejolu" },
-    { name: "Zuri Team", link: "https://training.zuri.team/" },
-    { name: "Zuri Books", link: "https://books.zuri.team" },
-    { name: "Python Books",link: "https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>"},
-    { name: "Background Check for Coders", link: "https://background.zuri.team"},
-    { name: "Design Books", link: "https://books.zuri.team/design-rules" },
+    { name: "Twitter Link", link: "https://twitter.com/TAdejolu", id:"twitter"},
+    { name: "Zuri Team", link: "https://training.zuri.team/", id:"btn__zuri" },
+    { name: "Zuri Books", link: "https://books.zuri.team", id:"books" },
+    { name: "Python Books",link: "https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>", id:"book__python" },
+    { name: "Background Check for Coders", link: "https://books.zuri.team/design-rules",id:"pitch"},
+    { name: "Design Books", link: "https://books.zuri.team/design-rules",id:"book__design" },
   ];
   return (
     <HoverEffectStyle>
       {links.map((i) => (
-        <a href={i.link}>
+        <a href={i.link} style={{textDecoration:"none",color:"black"}}>
           {" "}
-          <LinkCardStyle>{i.name}</LinkCardStyle>{" "}
+          <LinkCardStyle id={i.id}>{i.name}</LinkCardStyle>{" "}
         </a>
       ))}
     </HoverEffectStyle>
